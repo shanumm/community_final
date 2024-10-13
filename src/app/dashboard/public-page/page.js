6d9af80cd8f54145f15acfdc3592724bfd5507f2
Ekta Kaushik <ekta.k@purplle.com>
2024-10-13 20:24:03 +0530

    update page.js

--- src/app/dashboard/public-page/page.js
+++ src/app/dashboard/public-page/page.js
@@ -1,7 +1,6 @@
 import Chat_box from "@/custom_components/chat_box/chat_box";
 import React from "react";
 
-
 const Top_navigation = () => (
   <div>
     <ul className="border-b flex w-1/2 justify-between">
@@ -15,17 +14,16 @@ const Top_navigation = () => (
 
 export default function page() {
   return (
-    // <div className="flex border-2 border-red-700 h-screen">
-    //   <div className="w-56">testing</div>
-    //   <div className="border-2 border-red-800 flex-1 py-20 px-40">
-    //     <div>Settings</div>
-    //     <Top_navigation />
-    //     <div className="text-2xl">public page</div>
-    //     <div>
-    //       This page shows non-members basic information about your community.
-    //     </div>
-    //   </div>
-      <Chat_box/>
-    // </div>
+    <div className="flex border-2 border-red-700 h-screen">
+      <div className="w-56">testing</div>
+      <div className="border-2 border-red-800 flex-1 py-20 px-40">
+        <div>Settings</div>
+        <Top_navigation />
+        <div className="text-2xl">public page</div>
+        <div>
+          This page shows non-members basic information about your community.
+        </div>
+      </div>
+    </div>
   );
 }
